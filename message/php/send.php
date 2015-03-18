e74b1c26e5f3f60fd62312ce13ac5dc6899ab2f0
ChaitraMK <kanthichaitra@gmail.com>
2015-03-17 22:02:22 -0700

    Update send.php

--- message/php/send.php
+++ message/php/send.php
@@ -3,14 +3,14 @@
 require "../twilio/Services/Twilio.php";
 
 // set your AccountSid and AuthToken from www.twilio.com/user/account
-$AccountSid = "ACdcb07be670ddd2b5951cf0e6a265fbdd";
-$AuthToken  = "924d5d0fd53f84fc27e38b2dc522cc76";
+$AccountSid = "..................................................";
+$AuthToken  = ".......................................";
 
 $client = new Services_Twilio($AccountSid, $AuthToken);
 
 $message = $client->account->messages->create(array(
-		"From" => "8478071716",
-		"To"   => "8147264989",
+		"From" => "........",
+		"To"   => ".........",
 		"Body" => "Test message!",
 	));
 
