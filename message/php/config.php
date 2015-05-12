0395ab3c66b6001eafb1dd4158326ec4f4131738
ChaitraMK <kanthichaitra@gmail.com>
2015-05-11 23:14:02 -0700

    Update config.php

--- message/php/config.php
+++ message/php/config.php
@@ -1,6 +1,6 @@
 <?php
-$con = mysql_connect("localhost", "ardevelo_sai", "tatvamasi");
-$db  = mysql_select_db("ardevelo_saiprema", $con);
+$con = mysql_connect("localhost", ".......", "........");
+$db  = mysql_select_db("........", $con);
 date_default_timezone_set('Asia/Kolkata');
 mysql_set_charset('utf8', $con);
 ?>
