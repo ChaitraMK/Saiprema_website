00f0ea3858cd48ddbf7344b4cf97fd2863b6a616
ChaitraMK <kanthichaitra@gmail.com>
2015-05-13 23:36:47 -0700

    Create send.php

---
+++ message/php/send.php
@@ -0,0 +1,14 @@
+<?php
+require "../twilio/Services/Twilio.php";
+// set your AccountSid and AuthToken from www.twilio.com/user/account
+$AccountSid = "..................................................";
+$AuthToken  = ".......................................";
+$client = new Services_Twilio($AccountSid, $AuthToken);
+$message = $client->account->messages->create(array(
+		"From" => "........",
+		"To"   => ".........",
+		"Body" => "Test message!",
+	));
+// Display a confirmation message on the screen
+echo "Sent message {$message->sid}";
+?>
